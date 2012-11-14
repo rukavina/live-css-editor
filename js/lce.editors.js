1e5f34144556b1397a0e7fadb1f0df43b7030c88
Milan Rukavina <rukavinamilan@gmail.com>
2012-11-14 19:51:30 +0100

    initial import

---
+++ js/lce.editors.js
@@ -0,0 +1,25 @@
+(function(){
+    //default
+    $.fn.livecsseditor.setPropertyEditor('default',function defaultEditorCallback(options){
+        var html = '<form class="form-inline"><input type="text" value="" /><a class="btn" href="#"><i class=" icon-ok"></i></a></form>'
+        options.container.html(html);
+        options.container.find('input').val(options.value);
+        options.container.find('a.btn').click(function(){
+            options.setValue(options.container.find('input').val());
+            return false; 
+        });
+    }); 
+
+    //color
+    $.fn.livecsseditor.setPropertyEditor(['color','background-color'],function defaultEditorCallback(options){
+        var html = '<form class="form-inline"><div class="input-append color" data-color="' + options.value + '" data-color-format="rgb"><input type="text" value="' + options.value + '" /><span class="add-on"><i style="background-color: ' + options.value + '"></i></span></div><a class="btn" href="#"><i class=" icon-ok"></i></a></form>';
+        options.container.html(html);
+        options.container.find('div.color').colorpicker();
+        options.container.find('a.btn').click(function(){
+            options.setValue(options.container.find('input').val());
+            return false; 
+        });        
+    });  
+    
+})();
+
