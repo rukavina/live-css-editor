d849667f789582b15f657ced715c41a11b4f32b1
Douglas <machadoug@users.noreply.github.com>
2017-06-23 09:12:38 -0300

    Update lceColor.js

--- source/livecsseditor/editors/lceColor.js
+++ source/livecsseditor/editors/lceColor.js
@@ -6,7 +6,7 @@
  * @version 2.0
  */
 
-(function(){
+;(function($){
     var hexDigits = new Array
     ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
 
@@ -70,5 +70,4 @@
             vars.setValue(color);
         });
     });    
-})();
-
+})(jQuery);
